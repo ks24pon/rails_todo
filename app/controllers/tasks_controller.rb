468e690c61154e9e0539a449f328634f0bd65947
watanabereo <watanabe-re@ibjapan.jp>
2023-02-03 15:50:34 +0900

    index,new,create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,31 @@
 class TasksController < ApplicationController
+    # 一覧
     def index
       @tasks = Task.all
+    end
+    # 画面作成
+    def new
+      @task = Task.new
+    end
+    # 追加機能
+    def create
+      @task = Task.new(task_params)
+      @task.save
+      redirect_to tasks_path
+    end
+    # 編集画面
+    def edit
+      @task = Task.find(params[:id])
+    end
+    # 編集処理
+    def update
+      @task = Task.find(params[:id])
+      @task.update(task_params)
+      redirect_to tasks_path
+    end
+    # strong_parametr
+    private
+    def task_params
+      params.require(:task).permit(:title)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/tasks" => "tasks#index"
+  get "/tasks/new" => "tasks#new"
+  post "/tasks/create" => "tasks#create"
+  get "/tasks/:id/edit" => "tasks#edit", as: 'tasks_edit_path'
+  put "/tasks/:id" => "tasks#update", as: 'tasks_update_path'
 end
